1101dd42f2d694af3d86537ee981ac69625a8961
DaPorkchop_ <daporkchop@daporkchop.net>
2024-06-21 14:40:59 +0200

    macros: make_error_type supports struct-like enums as well as tuple enums

--- src/macros.rs
+++ src/macros.rs
@@ -5,16 +5,13 @@
 ///
 /// Example usage:
 /// ```ignore
-/// make_error_type!(pub MyError {
+/// make_error_type!(pub enum MyError {
 ///     Unknown; "Unknown error",
 ///     IncorrectLength(length: usize); "Incorrect length {length}, expected 123",
 ///     InvalidHexData(source: hex::FromHexError); "Cannot convert hex string: {source}"; source,
 /// });
 /// ```
 macro_rules! make_error_type {
-    (@fmt_args $tname:ident) => { Self::$tname };
-    (@fmt_args $tname:ident ( $($targn:ident),+ )) => { Self::$tname($($targn),+) };
-
     (@fmt_source) => { None };
     (@fmt_source $sourcen:expr) => { Some($sourcen) };
 
@@ -23,8 +20,10 @@ macro_rules! make_error_type {
         $vis:vis enum $name:ident {
             $( $(
                 #[$tattr:meta] )*
-                $tname:ident $(( $($targn:ident : $targ:ty),+ ))?
-                ; $tmsg:literal
+                $tname:ident
+                    $(( $( $( #[$t_tuple_arg_attr:meta] )* $t_tuple_arg_name:ident : $t_tuple_arg_ty:ty),+ $(,)? ))?
+                    $({ $( $( #[$t_struct_arg_attr:meta] )* $t_struct_arg_name:ident : $t_struct_arg_ty:ty),+ $(,)? })?
+                ; $tmsg:literal $(( $($tmsgarg:expr),* ))?
                 $( ; $sourcen:expr )?
             ),+ $(,)?
         }
@@ -34,15 +33,20 @@ macro_rules! make_error_type {
         $vis enum $name {
             $(
                 $( #[$tattr] )*
-                $tname $(( $($targ),+ ))?
+                $tname
+                    $(( $( $( #[$t_tuple_arg_attr] )* $t_tuple_arg_ty),+ ))?
+                    $({ $( $( #[$t_struct_arg_attr] )* $t_struct_arg_name : $t_struct_arg_ty),+ })?
             ),+
         }
 
         impl std::error::Error for $name {
+            #[allow(unused_variables)]
             fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
                 match self {
                     $(
-                        make_error_type!(@fmt_args $tname $(( $($targn),+ ))?)
+                        Self::$tname
+                            $(( $($t_tuple_arg_name),+ ))?
+                            $({ $($t_struct_arg_name),+ })?
                         =>
                         make_error_type!(@fmt_source $($sourcen)?)
                     ),+
@@ -54,9 +58,11 @@ macro_rules! make_error_type {
             fn fmt(&self, _f: &mut std::fmt::Formatter) -> std::fmt::Result {
                 match self {
                     $(
-                        make_error_type!(@fmt_args $tname $(( $($targn),+ ))?)
+                        Self::$tname
+                            $(( $($t_tuple_arg_name),+ ))?
+                            $({ $($t_struct_arg_name),+ })?
                         =>
-                        write!(_f, $tmsg)
+                        write!(_f, $tmsg $($(, $tmsgarg)*)? )
                     ),+
                 }
             }
